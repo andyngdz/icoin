850fd560f8df336336477cb70d2d93765c82b627
Andy D. Ng <itc.anhduy@gmail.com>
2021-02-02 20:14:54 +0700

    [ANDY] Add tests for header

--- components/assets/__tests__/Assets.test.tsx
+++ components/assets/__tests__/Assets.test.tsx
@@ -1,7 +1,5 @@
-import { asset } from 'mock-data'
 import { Assets } from 'components'
 import { act, customRender, RenderResult } from 'services/test-utils'
-import { Table, TableBody } from '@material-ui/core'
 
 describe('Assets', () => {
   it('Should render it correctly', async () => {
