859d618c9335418e8270469148c6a59cffdcf3ae
Andy D. Ng <itc.anhduy@gmail.com>
2021-02-03 11:08:46 +0700

    [ANDY] Add interface for asset summary

--- components/asset/AssetSummary.tsx
+++ components/asset/AssetSummary.tsx
@@ -1,19 +1,28 @@
-import { Container } from '@material-ui/core'
-import { useQuery, COIN_INFORMATION } from 'apollo'
+import { Container, LinearProgress } from '@material-ui/core'
 import { IAssetSummary } from 'types'
+import { SymbolIcon } from 'components'
+import { useQuery, COIN_INFORMATION } from 'apollo'
 
 interface IAssetSummaryProps {
   id: string
 }
 
 const AssetSummary: React.FC<IAssetSummaryProps> = ({ id }) => {
-  useQuery<IAssetSummary>(COIN_INFORMATION, {
+  const { loading, data } = useQuery<IAssetSummary>(COIN_INFORMATION, {
     variables: { id }
   })
 
+  if (loading) {
+    return <LinearProgress />
+  }
+
+  const { name, symbol } = data.asset
+
   return (
     <Container>
-      <div></div>
+      <div>
+        <SymbolIcon name={name} symbol={symbol} />
+      </div>
     </Container>
   )
 }
