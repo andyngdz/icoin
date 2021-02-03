151713fde47860a9b0769f0ba81201239b783128
Andy D. Ng <itc.anhduy@gmail.com>
2021-02-03 13:46:47 +0700

    [ANDY] Add summary information

--- components/asset/AssetSummary.tsx
+++ components/asset/AssetSummary.tsx
@@ -1,30 +1,109 @@
-import { Container, LinearProgress } from '@material-ui/core'
+import {
+  Container,
+  Typography,
+  Button,
+  Link,
+  makeStyles
+} from '@material-ui/core'
+import { Format } from 'services'
 import { IAssetSummary } from 'types'
-import { SymbolIcon } from 'components'
+import { Render } from 'use-react-common'
 import { useQuery, COIN_INFORMATION } from 'apollo'
 
 interface IAssetSummaryProps {
   id: string
 }
 
+const useStyles = makeStyles(
+  theme => ({
+    wrapper: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+      backgroundColor: theme.palette.primary.light
+    },
+
+    container: {
+      color: theme.palette.common.white
+    },
+
+    information: {
+      display: 'flex',
+      alignItems: 'flex-start',
+      justifyContent: 'space-between'
+    },
+
+    firstRow: {
+      paddingBottom: theme.spacing(2)
+    }
+  }),
+  {
+    name: 'AssetSummary'
+  }
+)
+
 const AssetSummary: React.FC<IAssetSummaryProps> = ({ id }) => {
-  const { loading, data } = useQuery<IAssetSummary>(COIN_INFORMATION, {
+  const classes = useStyles()
+  const { data } = useQuery<IAssetSummary>(COIN_INFORMATION, {
     variables: { id }
   })
 
-  if (loading) {
-    return <LinearProgress />
-  }
+  return Render.ensure(summary => {
+    const {
+      explorer,
+      marketCapUsd,
+      name,
+      priceUsd,
+      supply,
+      symbol,
+      volumeUsd24Hr,
+      website
+    } = summary.asset
 
-  const { name, symbol } = data.asset
-
-  return (
-    <Container>
-      <div>
-        <SymbolIcon name={name} symbol={symbol} />
+    return (
+      <div className={classes.wrapper}>
+        <Container className={classes.container}>
+          <summary className={classes.information}>
+            <section>
+              <Typography variant="h3" className={classes.firstRow}>
+                {name} ({symbol})
+              </Typography>
+              <Typography variant="h5">{Format.currency(priceUsd)}</Typography>
+            </section>
+            <section>
+              <div className={classes.firstRow}>
+                <Typography variant="button">Market Cap</Typography>
+                <Typography variant="h5">
+                  {Format.bigNumber(marketCapUsd)}
+                </Typography>
+              </div>
+              <Link href={website} color="inherit" target="_blank">
+                <Button color="inherit" variant="outlined">
+                  Website
+                </Button>
+              </Link>
+            </section>
+            <section>
+              <div className={classes.firstRow}>
+                <Typography variant="button">Volume (24h)</Typography>
+                <Typography variant="h5">
+                  {Format.bigNumber(volumeUsd24Hr)}
+                </Typography>
+              </div>
+              <Link href={explorer} color="inherit" target="_blank">
+                <Button color="inherit" variant="outlined">
+                  Explorer
+                </Button>
+              </Link>
+            </section>
+            <section>
+              <Typography variant="button">Supply</Typography>
+              <Typography variant="h5">{Format.bigNumber(supply)}</Typography>
+            </section>
+          </summary>
+        </Container>
       </div>
-    </Container>
-  )
+    )
+  }, data)
 }
 
 export { AssetSummary }
