e47b344f4758b7a832d2344f61799e7cbd13deff
Andy D. Ng <itc.anhduy@gmail.com>
2021-02-03 14:36:28 +0700

    [ANDY] Update usePriceDirection

--- components/asset/AssetSummary.tsx
+++ components/asset/AssetSummary.tsx
@@ -9,6 +9,7 @@ import { Format } from 'services'
 import { IAssetSummary } from 'types'
 import { Render } from 'use-react-common'
 import { useQuery, COIN_INFORMATION } from 'apollo'
+import { usePriceDirection } from 'components'
 
 interface IAssetSummaryProps {
   id: string
@@ -42,68 +43,73 @@ const useStyles = makeStyles(
 )
 
 const AssetSummary: React.FC<IAssetSummaryProps> = ({ id }) => {
-  const classes = useStyles()
   const { data } = useQuery<IAssetSummary>(COIN_INFORMATION, {
     variables: { id }
   })
 
-  return Render.ensure(summary => {
-    const {
-      explorer,
-      marketCapUsd,
-      name,
-      priceUsd,
-      supply,
-      symbol,
-      volumeUsd24Hr,
-      website
-    } = summary.asset
+  return Render.ensure(summary => <AssetSummaryContent {...summary} />, data)
+}
 
-    return (
-      <div className={classes.wrapper}>
-        <Container className={classes.container}>
-          <summary className={classes.information}>
-            <section>
-              <Typography variant="h3" className={classes.firstRow}>
-                {name} ({symbol})
+const AssetSummaryContent: React.FC<IAssetSummary> = ({ asset }) => {
+  const {
+    id,
+    explorer,
+    marketCapUsd,
+    name,
+    priceUsd,
+    supply,
+    symbol,
+    volumeUsd24Hr,
+    website
+  } = asset
+  const classes = useStyles()
+  const { price } = usePriceDirection(id, priceUsd)
+
+  return (
+    <div className={classes.wrapper}>
+      <Container className={classes.container}>
+        <summary className={classes.information}>
+          <section>
+            <Typography variant="h3" className={classes.firstRow}>
+              {name} ({symbol})
+            </Typography>
+            <Typography variant="h5">{Format.currency(price)}</Typography>
+          </section>
+          <section>
+            <div className={classes.firstRow}>
+              <Typography variant="button">Market Cap</Typography>
+              <Typography variant="h5">
+                {Format.bigNumber(marketCapUsd)}
               </Typography>
-              <Typography variant="h5">{Format.currency(priceUsd)}</Typography>
-            </section>
-            <section>
-              <div className={classes.firstRow}>
-                <Typography variant="button">Market Cap</Typography>
-                <Typography variant="h5">
-                  {Format.bigNumber(marketCapUsd)}
-                </Typography>
-              </div>
-              <Link href={website} color="inherit" target="_blank">
-                <Button color="inherit" variant="outlined">
-                  Website
-                </Button>
-              </Link>
-            </section>
-            <section>
-              <div className={classes.firstRow}>
-                <Typography variant="button">Volume (24h)</Typography>
-                <Typography variant="h5">
-                  {Format.bigNumber(volumeUsd24Hr)}
-                </Typography>
-              </div>
-              <Link href={explorer} color="inherit" target="_blank">
-                <Button color="inherit" variant="outlined">
-                  Explorer
-                </Button>
-              </Link>
-            </section>
-            <section>
-              <Typography variant="button">Supply</Typography>
-              <Typography variant="h5">{Format.bigNumber(supply)}</Typography>
-            </section>
-          </summary>
-        </Container>
-      </div>
-    )
-  }, data)
+            </div>
+            <Link href={website} color="inherit" target="_blank">
+              <Button color="inherit" variant="outlined">
+                Website
+              </Button>
+            </Link>
+          </section>
+          <section>
+            <div className={classes.firstRow}>
+              <Typography variant="button">Volume (24h)</Typography>
+              <Typography variant="h5">
+                {Format.bigNumber(volumeUsd24Hr)}
+              </Typography>
+            </div>
+            <Link href={explorer} color="inherit" target="_blank">
+              <Button color="inherit" variant="outlined">
+                Explorer
+              </Button>
+            </Link>
+          </section>
+          <section>
+            <Typography variant="button">Supply</Typography>
+            <Typography variant="h5">{Format.bigNumber(supply)}</Typography>
+          </section>
+        </summary>
+      </Container>
+    </div>
+  )
 }
 
 export { AssetSummary }
+export { AssetSummaryContent }
--- components/hooks/usePriceDirection.ts
+++ components/hooks/usePriceDirection.ts
@@ -6,14 +6,14 @@ import { useState, useEffect } from 'react'
 
 const usePriceDirection = (id: string, initPrice: number) => {
   const [direction, setDirection] = useState(PriceDirection.VOID)
-  const livePrice = useSelector((store: IRootStore) => store.prices.data[id])
-  const price = livePrice || initPrice
+  const [price, setPrice] = useState(initPrice)
+  const prices = useSelector((store: IRootStore) => store.prices.data)
+  const livePrice = prices[id]
 
   useEffect(() => {
-    if (livePrice > initPrice) {
-      setDirection(PriceDirection.UP)
-    } else {
-      setDirection(PriceDirection.DOWN)
+    if (livePrice && livePrice !== price) {
+      setDirection(livePrice > price ? PriceDirection.UP : PriceDirection.DOWN)
+      setPrice(livePrice)
     }
 
     const clearDirectionTimeoutId = setTimeout(
@@ -22,7 +22,7 @@ const usePriceDirection = (id: string, initPrice: number) => {
     )
 
     return () => clearTimeout(clearDirectionTimeoutId)
-  }, [livePrice, initPrice])
+  }, [livePrice, price])
 
   return { direction, price }
 }
