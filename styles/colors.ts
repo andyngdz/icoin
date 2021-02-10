0d54074cb3471c5db140969f19eb60050f22d8dc
Andy D. Ng <itc.anhduy@gmail.com>
2021-02-10 23:43:11 +0700

    [ANDY] Update icon, styling

--- styles/colors.ts
+++ styles/colors.ts
@@ -2,6 +2,7 @@ export const secondary = '#2C292D'
 export const secondaryLight = '#555156'
 export const secondaryDark = '#000002'
 
+export const success = '#28A745'
 export const divider = '#EFF2F5'
 export const gray1 = '#333333'
 export const white = '#FFFFFF'
