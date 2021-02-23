9284a9ba6d73071f8bae754b1c57f6f4e20b7743
Andy D. Ng <itc.anhduy@gmail.com>
2021-02-23 11:22:50 +0700

    [ANDY] Fade in background

--- components/search/Search.tsx
+++ components/search/Search.tsx
@@ -3,9 +3,13 @@ import {
   TextField,
   InputAdornment,
   IconButton,
+  ClickAwayListener,
+  Fade,
+  fade,
   makeStyles
 } from '@material-ui/core'
 import { ContainerWrapper } from 'components'
+import { useState } from 'react'
 import SearchIcon from '@material-ui/icons/Search'
 
 const useStyles = makeStyles(
@@ -13,6 +17,20 @@ const useStyles = makeStyles(
     search: {
       paddingTop: theme.spacing(5),
       paddingBottom: theme.spacing(5)
+    },
+
+    input: {
+      zIndex: 1102
+    },
+
+    overlay: {
+      position: 'fixed',
+      backgroundColor: fade(theme.palette.common.white, 0.75),
+      zIndex: 1101,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
     }
   }),
   {
@@ -22,26 +40,36 @@ const useStyles = makeStyles(
 
 const Search = (): React.ReactElement => {
   const classes = useStyles()
+  const [touch, setTouch] = useState(false)
 
   return (
-    <Paper className={classes.search} square>
-      <ContainerWrapper>
-        <TextField
-          placeholder="Search for cryptocurrencies"
-          variant="outlined"
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-          fullWidth
-        />
-      </ContainerWrapper>
-    </Paper>
+    <section>
+      <Paper className={classes.search} square>
+        <ContainerWrapper>
+          <ClickAwayListener onClickAway={() => setTouch(false)}>
+            <TextField
+              placeholder="Search for cryptocurrencies"
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton>
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+              onClick={() => setTouch(true)}
+              className={classes.input}
+              fullWidth
+            />
+          </ClickAwayListener>
+        </ContainerWrapper>
+      </Paper>
+      <Fade in={touch}>
+        <div className={classes.overlay} />
+      </Fade>
+    </section>
   )
 }
 
