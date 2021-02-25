7a5a1fb7459f3e86f4b1aed475335f7009528df1
Andy Ng <itc.anhduy@gmail.com>
2021-02-25 16:57:36 +0700

    [ANDY] Add tests

--- components/footer/FollowUs.tsx
+++ components/footer/FollowUs.tsx
@@ -1,31 +1,36 @@
-import { Grid, IconButton } from '@material-ui/core'
+import { Typography, Grid, IconButton } from '@material-ui/core'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import InstagramIcon from '@material-ui/icons/Instagram'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import YouTubeIcon from '@material-ui/icons/YouTube'
 
-const FollowUs = (): React.ReactElement => {
+const FollowUs: React.FC = (): React.ReactElement => {
   return (
-    <Grid spacing={1} container>
+    <Grid justify="flex-end" container>
       <Grid item>
-        <IconButton size="small">
-          <FacebookIcon />
-        </IconButton>
+        <Typography variant="button">FOLLOW US</Typography>
       </Grid>
-      <Grid item>
-        <IconButton>
-          <TwitterIcon size="small" />
-        </IconButton>
-      </Grid>
-      <Grid item>
-        <IconButton>
-          <InstagramIcon size="small" />
-        </IconButton>
-      </Grid>
-      <Grid item>
-        <IconButton>
-          <YouTubeIcon size="small" />
-        </IconButton>
+      <Grid spacing={1} justify="flex-end" container>
+        <Grid item>
+          <IconButton size="small">
+            <FacebookIcon />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <IconButton size="small">
+            <TwitterIcon />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <IconButton size="small">
+            <InstagramIcon />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <IconButton size="small">
+            <YouTubeIcon />
+          </IconButton>
+        </Grid>
       </Grid>
     </Grid>
   )
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,86 +7,88 @@ const Footer = (): React.ReactElement => {
     <PaperWrapper square>
       <ContainerWrapper>
         <Grid spacing={3} direction="row" container>
-          <Grid xs={3} item>
-            <Grid spacing={1} direction="column" container>
-              <Grid item>
-                <Typography variant="button">ICoin</Typography>
+          <Grid xs={8} item>
+            <Grid direction="row" container>
+              <Grid xs={4} item>
+                <Grid spacing={1} direction="column" container>
+                  <Grid item>
+                    <Typography variant="button">ICoin</Typography>
+                  </Grid>
+                  <Grid item>
+                    <NextLink href="#" passHref>
+                      <Link>
+                        <Typography variant="body2">Methodology</Typography>
+                      </Link>
+                    </NextLink>
+                  </Grid>
+                  <Grid item>
+                    <NextLink href="#" passHref>
+                      <Link>
+                        <Typography variant="body2">About Us</Typography>
+                      </Link>
+                    </NextLink>
+                  </Grid>
+                  <Grid item>
+                    <NextLink href="#" passHref>
+                      <Link>
+                        <Typography variant="body2">Support</Typography>
+                      </Link>
+                    </NextLink>
+                  </Grid>
+                  <Grid item>
+                    <NextLink href="#" passHref>
+                      <Link>
+                        <Typography variant="body2">Our API</Typography>
+                      </Link>
+                    </NextLink>
+                  </Grid>
+                  <Grid item>
+                    <NextLink href="#" passHref>
+                      <Link>
+                        <Typography variant="body2">Rate Comparison</Typography>
+                      </Link>
+                    </NextLink>
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid item>
-                <NextLink href="#" passHref>
-                  <Link>
-                    <Typography variant="body2">Methodology</Typography>
-                  </Link>
-                </NextLink>
+              <Grid xs={4} item>
+                <Grid spacing={1} direction="column" container>
+                  <Grid item>
+                    <Typography variant="button">Legals</Typography>
+                  </Grid>
+                  <Grid item>
+                    <NextLink href="#" passHref>
+                      <Link>
+                        <Typography variant="body2">
+                          Terms of Service
+                        </Typography>
+                      </Link>
+                    </NextLink>
+                  </Grid>
+                  <Grid item>
+                    <NextLink href="#" passHref>
+                      <Link>
+                        <Typography variant="body2">Privacy Policy</Typography>
+                      </Link>
+                    </NextLink>
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid item>
-                <NextLink href="#" passHref>
-                  <Link>
-                    <Typography variant="body2">About Us</Typography>
-                  </Link>
-                </NextLink>
-              </Grid>
-              <Grid item>
-                <NextLink href="#" passHref>
-                  <Link>
-                    <Typography variant="body2">Support</Typography>
-                  </Link>
-                </NextLink>
-              </Grid>
-              <Grid item>
-                <NextLink href="#" passHref>
-                  <Link>
-                    <Typography variant="body2">Our API</Typography>
-                  </Link>
-                </NextLink>
-              </Grid>
-              <Grid item>
-                <NextLink href="#" passHref>
-                  <Link>
-                    <Typography variant="body2">Rate Comparison</Typography>
-                  </Link>
-                </NextLink>
+              <Grid xs={4} item>
+                <Grid spacing={1} direction="column" container>
+                  <Grid item>
+                    <Typography variant="button">DISCLAIMER</Typography>
+                  </Grid>
+                  <Grid item>
+                    ICoin is an open-source project for tracking
+                    cryptocurrencies
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid xs={3} item>
-            <Grid spacing={1} direction="column" container>
-              <Grid item>
-                <Typography variant="button">Legals</Typography>
-              </Grid>
-              <Grid item>
-                <NextLink href="#" passHref>
-                  <Link>
-                    <Typography variant="body2">Terms of Service</Typography>
-                  </Link>
-                </NextLink>
-              </Grid>
-              <Grid item>
-                <NextLink href="#" passHref>
-                  <Link>
-                    <Typography variant="body2">Privacy Policy</Typography>
-                  </Link>
-                </NextLink>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid xs={3} item>
-            <Grid spacing={1} direction="column" container>
-              <Grid item>
-                <Typography variant="button">DISCLAIMER</Typography>
-              </Grid>
-              <Grid item>
-                ICoin is an open-source project for tracking cryptocurrencies
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid xs={3} item>
-            <Grid spacing={1} direction="column" container>
-              <Grid item>
-                <Typography variant="button">FOLLOW US</Typography>
-              </Grid>
-              <FollowUs />
-            </Grid>
+          <Grid xs={4} item>
+            <FollowUs />
           </Grid>
         </Grid>
       </ContainerWrapper>
