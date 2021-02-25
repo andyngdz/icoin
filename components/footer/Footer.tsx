9d14970032b2e8a400f753df45f1aeccec366ab7
Andy Ng <itc.anhduy@gmail.com>
2021-02-25 13:46:46 +0700

    [ANDY] Add link

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,51 +1,95 @@
-import { Grid, Typography } from '@material-ui/core'
+import { ContainerWrapper, PaperWrapper } from 'components'
+import { Grid, Typography, Link } from '@material-ui/core'
+import NextLink from 'next/link'
 
 const Footer = (): React.ReactElement => {
   return (
-    <Grid container>
-      <Grid container>
-        <Grid item>
-          <Typography variant="button">ICoin</Typography>
+    <PaperWrapper square>
+      <ContainerWrapper>
+        <Grid spacing={3} direction="row" container>
+          <Grid xs={3} item>
+            <Grid spacing={1} direction="column" container>
+              <Grid item>
+                <Typography variant="button">ICoin</Typography>
+              </Grid>
+              <Grid item>
+                <NextLink href="#" passHref>
+                  <Link>
+                    <Typography variant="body2">Methodology</Typography>
+                  </Link>
+                </NextLink>
+              </Grid>
+              <Grid item>
+                <NextLink href="#" passHref>
+                  <Link>
+                    <Typography variant="body2">About Us</Typography>
+                  </Link>
+                </NextLink>
+              </Grid>
+              <Grid item>
+                <NextLink href="#" passHref>
+                  <Link>
+                    <Typography variant="body2">Support</Typography>
+                  </Link>
+                </NextLink>
+              </Grid>
+              <Grid item>
+                <NextLink href="#" passHref>
+                  <Link>
+                    <Typography variant="body2">Our API</Typography>
+                  </Link>
+                </NextLink>
+              </Grid>
+              <Grid item>
+                <NextLink href="#" passHref>
+                  <Link>
+                    <Typography variant="body2">Rate Comparison</Typography>
+                  </Link>
+                </NextLink>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={3} item>
+            <Grid spacing={1} direction="column" container>
+              <Grid item>
+                <Typography variant="button">Legals</Typography>
+              </Grid>
+              <Grid item>
+                <NextLink href="#" passHref>
+                  <Link>
+                    <Typography variant="body2">Terms of Service</Typography>
+                  </Link>
+                </NextLink>
+              </Grid>
+              <Grid item>
+                <NextLink href="#" passHref>
+                  <Link>
+                    <Typography variant="body2">Privacy Policy</Typography>
+                  </Link>
+                </NextLink>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={3} item>
+            <Grid spacing={1} direction="column" container>
+              <Grid item>
+                <Typography variant="button">DISCLAIMER</Typography>
+              </Grid>
+              <Grid item>
+                ICoin is an open-source project for tracking cryptocurrencies
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={3} item>
+            <Grid spacing={1} direction="column" container>
+              <Grid item>
+                <Typography variant="button">FOLLOW US</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Typography variant="caption">Methodology</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="caption">About Us</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="caption">Support</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="caption">Our API</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="caption">Rate Comparison</Typography>
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item>
-          <Typography variant="button">Legals</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="caption">Terms of Service</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="caption">Privacy Policy</Typography>
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item>
-          <Typography variant="button">DISCLAIMER</Typography>
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item>
-          <Typography variant="button">FOLLOW US</Typography>
-        </Grid>
-      </Grid>
-      <Grid container></Grid>
-    </Grid>
+      </ContainerWrapper>
+    </PaperWrapper>
   )
 }
 
